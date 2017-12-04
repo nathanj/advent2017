f8bd32687f81d12e3041360022cc9bff3c511377
Nathan Jones <nathanj439@gmail.com>
2017-12-04 08:45:10 -0500

    formatting

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,16 @@
 import kotlin.system.measureTimeMillis
 
 fun timeit(block: () -> Unit) {
-	val time = measureTimeMillis(block)
-	println("Time taken: $time ms")
+    val time = measureTimeMillis(block)
+    println("Time taken: $time ms")
 }
 
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-	    //timeit { advent.day1() }
-	    //timeit { advent.day2() }
-	    //timeit { advent.day3() }
+        //timeit { advent.day1() }
+        //timeit { advent.day2() }
+        //timeit { advent.day3() }
         timeit { advent.day4() }
     }
 }
